ec04b94d8b59899c4a5c616142825154e968442c
Raúl Pampliega <raul.pampliega@anjanadata.com>
2021-11-27 23:53:38 +0100

    actualizar ddl

--- src/test/resources/ddl_test.sql
+++ src/test/resources/ddl_test.sql
@@ -3,3 +3,6 @@ ALTER SEQUENCE sc_pk_entity RESTART WITH (select max(id_entity) from entity) + 1
 
 CREATE SEQUENCE IF NOT EXISTS sc_pk_relationship START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
 ALTER SEQUENCE sc_pk_relationship RESTART WITH (select max(id_relationship) from relationship) + 1;
+
+CREATE SEQUENCE IF NOT EXISTS sc_pk_attributeValue START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+ALTER SEQUENCE sc_pk_attributeValue RESTART WITH (select max(id_attribute_value) from attribute_value) + 1;
