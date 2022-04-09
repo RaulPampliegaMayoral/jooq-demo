200fbc8aed9197dad9739eb56a62e36877f1f531
Raúl Pampliega <raul.pampliega@anjanadata.com>
2022-04-09 20:59:28 +0200

    simplificar tests

--- src/test/java/com/anjana/raulpampliega/jooqdemo/JinqTest.java
+++ src/test/java/com/anjana/raulpampliega/jooqdemo/JinqTest.java
@@ -1,70 +1,11 @@
 package com.anjana.raulpampliega.jooqdemo;
 
-import com.anjana.core.customers.enums.State;
-import com.anjana.raulpampliega.jooqdemo.model.AttributeValue;
-import com.anjana.raulpampliega.jooqdemo.model.Entity;
-import com.anjana.raulpampliega.jooqdemo.model.RelationShip;
 import com.anjana.raulpampliega.jooqdemo.repositories.CustomRepository;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import lombok.NoArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-@Log4j2
-@NoArgsConstructor
-@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, ExecutionTimeTestListener.class})
-public class JinqTest {
+public class JinqTest extends Base {
 
-  @Autowired
-  @Qualifier("JINQRepositoryImpl")
-  CustomRepository repository;
-
-  @Test
-  void getEntities() {
-    List<Integer> idsToFind = Arrays.asList(7,8,9);
-    List<Integer> entities = repository.getEntityByIds(idsToFind).stream()
-        .map(Entity::getId).collect(Collectors.toList());
-
-    Assertions.assertTrue(idsToFind.size() == entities.size());
-    Assertions.assertTrue(idsToFind.containsAll(entities));
-    Assertions.assertTrue(entities.containsAll(idsToFind));
-  }
-
-  @Test
-  void getAllDSAFromSourceRelationship() {
-    List<RelationShip> relations = repository.findRelationByInputObject(256, "DSA");
-    Assertions.assertNotNull(relations);
-    List<Integer> expected = Arrays.asList(174, 177);
-    List<Integer> actual = relations.stream().map(RelationShip::getId).collect(Collectors.toList());
-    Assertions.assertTrue(expected.size() == actual.size());
-    Assertions.assertTrue(expected.containsAll(actual));
-    Assertions.assertTrue(actual.containsAll(expected));
-  }
-
-  @Test
-  void getRelationsShipDatasetFlags() {
-    List<RelationShip> states = repository.getDatasetFlags("daniel.ruiz",251);
-    Assertions.assertNotNull(states);
-    Assertions.assertIterableEquals(Arrays.asList(State.APPROVED), states.stream().map(RelationShip::getState).collect(Collectors.toList()));
-  }
-
-  @Test
-  void getAttributeValues() {
-    List<AttributeValue> values = repository.getAllAttributeValues();
-    Assertions.assertNotNull(values);
-    Assertions.assertTrue(values.size() == 1500);
+  public JinqTest(@Qualifier("JINQRepositoryImpl") CustomRepository repository) {
+    super(repository);
   }
 }
--- src/test/java/com/anjana/raulpampliega/jooqdemo/JooqDemoApplicationTests.java
+++ src/test/java/com/anjana/raulpampliega/jooqdemo/JooqDemoApplicationTests.java
@@ -1,7 +1,5 @@
 package com.anjana.raulpampliega.jooqdemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
